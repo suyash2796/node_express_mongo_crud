0b8d552770bc3041af2440e6b1e420a242963a5d
suyash shrivastava <suyash2796@gmail.com>
2021-06-30 10:50:15 +0530

    app get and post on mongo

--- src/app/route/note_routes.js
+++ src/app/route/note_routes.js
@@ -1,11 +1,26 @@
-module.exports = function(app, db) {  
-    app.post('/notes', (req, res) => {  
-          // You'll create your note here. 
-            console.log(req.body);
-             res.send('Hello')
-            }
-             );
-            };
+var ObjectID = require('mongodb').ObjectID;
+module.exports = function(app, db){
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id)  };
+        db.collection('notes').findOne(details, (err, item) => { 
+                if (err)
+                    res.send({'error':'An error has occurred'});
+                else
+                    res.send(item);
+                });
+            });
+    const collection =   app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title }; 
+        db.collection('notes').insert(note, (err, result) =>
+        {
+        if (err)
+            res.send({ 'error': 'An error has occurred' });
+        else
+            res.send(result.ops[0]);
+        });  
+    });
+};
 
 
             
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db  = require('./app/config/db');
 
 const app = express();
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/route')(app, {});
+MongoClient.connect(db.url, (err, database) => {  
+    if (err) 
+        return console.log(err);
+        require('./app/route')(app, database);
+});
+// const database = database.db("NODEEXPRESSMONGOCRUD");
+
+
 app.listen(port, () => {  console.log('We are live on ' + port);});
